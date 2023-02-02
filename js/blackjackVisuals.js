7f8f0b56b38860dc3efc979f30a6f96e5b6de5cc
Alex <mcbuzzer@gmail.com>
2023-02-02 16:57:43 -0700

    BlackJack frontend work, everything but rules done

--- js/blackjackVisuals.js
+++ js/blackjackVisuals.js
@@ -1,11 +1,59 @@
 const playerHand = document.getElementById("playerHand")
-let playerHandCardCount = 1
+let playerHandCardCount = 1; // 1 because the first card is already there, Possibly change this to 0 once code works
+const dealerHand = document.getElementById("dealerHand")
+let dealerHandCardCount = 1; // 1 because the first card is already there, Possibly change this to 0 once code works
+
+let isStood = false;
+
 const hitButtonHandler = () => {
-    let newCard = document.createElement("img")
-    newCard.src = "../Assets/Cards/cardClubs2.png"
-    newCard.classList.add("faceUp")
-    newCard.style.transform = `rotate(${Math.max(15 - (playerHandCardCount * 3), 0)}deg)`
-    newCard.style.right = `-${20 * (playerHandCardCount * 2)}px`
-    playerHand.appendChild(newCard)
-    playerHandCardCount++
+    if (playerHandCardCount < 11 && !isStood) {
+        let newCard = document.createElement("img")
+        newCard.src = "../Assets/Cards/cardClubs2.png"
+        newCard.classList.add("faceUp")
+        newCard.style.transform = `rotate(${Math.max(20 - (playerHandCardCount * 3), 0)}deg)`
+        newCard.style.right = `-${20 * (playerHandCardCount * 1.3)}px`
+        playerHand.appendChild(newCard)
+        playerHandCardCount++
+
+        let newDealerCard = document.createElement("img")
+        newDealerCard.src = "../Assets/Cards/cardClubs6.png"
+        newDealerCard.classList.add("faceUp")
+        newDealerCard.style.transform = `rotate(${Math.max(20 - (dealerHandCardCount * 3), 0)}deg)`
+        newDealerCard.style.right = `-${20 * (dealerHandCardCount * 1.3)}px`
+        dealerHand.appendChild(newDealerCard)
+        dealerHandCardCount++
+    } else {
+        // New game?
+    }
+}
+
+const betDisplay = document.getElementById("betDisplay")
+let tokenCount = 0;
+const tokenDisplayHandler = (bet) => {
+    let newChip = document.createElement("img")
+    newChip.src = `../Assets/Overall UI/Chip${bet}.png`
+    newChip.classList.add("chip")
+    newChip.style.position = "absolute"
+    newChip.style.right = `-${tokenCount * 20}px`
+    betDisplay.appendChild(newChip)
+    tokenCount++
+}
+
+const handleBack = () => {
+    window.location.href = "../html/home.html"
+}
+
+const playerFaceDownCard = document.getElementById("playerFaceDownCard")
+const dealerFaceDownCard = document.getElementById("dealerFaceDownCard")
+const faceUpCards = document.getElementsByClassName("faceUp")
+const standButtonHandler = () => {
+    playerFaceDownCard.style.setProperty("--pathToFaceUpCard", "url(../Assets/Cards/cardClubs2.png)")
+    playerFaceDownCard.style.animation = "revealCard 1s forwards";
+    dealerFaceDownCard.style.setProperty("--pathToFaceUpCard", "url(../Assets/Cards/cardClubs5.png)")
+    dealerFaceDownCard.style.animation = "revealCard 1s forwards 0.75s";
+    for (let index = 0; index < faceUpCards.length; index++) {
+        faceUpCards[index].style.transition = `${0.3 + ((faceUpCards.length - index)/10)}s`;
+        faceUpCards[index].style.transform = "rotate(0deg)";
+    }
+    isStood = true;
 }
