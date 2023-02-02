e5c09a62c9ce02700d08ee90f7052b383bec7893
Alex <mcbuzzer@gmail.com>
2023-02-02 14:23:59 -0700

    blackjack visuals updated

--- js/blackjackVisuals.js
+++ js/blackjackVisuals.js
@@ -1,4 +1,11 @@
-const playerDeck = document.getElementById("")
+const playerHand = document.getElementById("playerHand")
+let playerHandCardCount = 1
 const hitButtonHandler = () => {
-    
+    let newCard = document.createElement("img")
+    newCard.src = "../Assets/Cards/cardClubs2.png"
+    newCard.classList.add("faceUp")
+    newCard.style.transform = `rotate(${Math.max(15 - (playerHandCardCount * 3), 0)}deg)`
+    newCard.style.right = `-${20 * (playerHandCardCount * 2)}px`
+    playerHand.appendChild(newCard)
+    playerHandCardCount++
 }
